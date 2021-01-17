1ce114f63dde3004a200f3fb285a209da8a444c7
sarahhyojin <sarahhyojin@gmail.com>
2021-01-17 13:45:05 +0900

    API

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, jsonify, request
 import requests
-from bs4 import BeautifulSoup
 from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+import datetime
+from datetime import timedelta
+import json
+import xmltodict
 
 app = Flask(__name__)
 
@@ -14,34 +17,75 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/weather', methods=['POST'])
-def post_weather():
-    # 1. 날씨 스크래핑하기
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    url = https://weather.naver.com/today/09620101
-    image = soup.select_one('# content > div > div.card.card_today > div.today_weather > div.weather_area > strong')
-    mise = soup.select_one('#content > div > div.card.card_today > div.today_weather > ul > li:nth-child(1) > a > div > em')
-    ultra_mise = soup.select_one('#content > div > div.card.card_today > div.today_weather > ul > li:nth-child(2) > a > div > em')
-
-    weather = {'url': url, 'image': image, 'mise': mise, 'ultra_mise': ultra_mise}
-
-    # 3. mongoDB에 데이터를 넣기
-    db.weathers.insert_one(weather)
-
-    return jsonify({'result': 'success'})
-
-
 @app.route('/weather', methods=['GET'])
-def read_articles():
-    # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기(Read)
-    result = list(db.weathers.find({}, {'_id': 0}))
-    # 2. weathers라는 키 값으로 articles 정보 보내주기
-    return jsonify({'result': 'success', 'weathers': result})
+def post_weather():
+    service_key = "BAsEIVf9MMHmkMVeeuK%2FqFA6vCoojczmlHEhH7OBVKazC8UYA1euOJbbT6R3xEQWo12Dc2sH3JIvg0Jj75bcQA%3D%3D"
+    pageNo = 1
+    numOfRows = 10
+    dataType = "JSON"
+    today = datetime.datetime.now().date().strftime("%Y%m%d")
+    yesterday = (datetime.datetime.now().date() - datetime.timedelta(1)).strftime("%Y%m%d")
+    now = datetime.datetime.now().time().strftime("%H%M")
+    if now < "0210":
+        base_date = yesterday
+        base_time = "2300"
+    elif now < "0510":
+        base_date = today
+        base_time = "0200"
+    elif now < "0810":
+        base_date = today
+        base_time = "0500"
+    elif now < "1110":
+        base_date = today
+        base_time = "0800"
+    elif now < "1410":
+        base_date = today
+        base_time = "1100"
+    elif now < "1710":
+        base_date = today
+        base_time = "1400"
+    elif now < "2010":
+        base_date = today
+        base_time = "1700"
+    elif now < "2310":
+        base_date = today
+        base_time = "2000"
+    elif now >= "2310":
+        base_date = today
+        base_time = "2300"
+    nx = 59
+    ny = 125
 
+    host = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst"
+    url = f"{host}?serviceKey={service_key}&pageNo={pageNo}&numOfRows={numOfRows}&dataType={dataType}&base_date={base_date}&base_time={base_time}&nx={nx}&ny={ny}"
+    response = requests.get(url)
+    response_data = response.json()
+    return jsonify(response_data)
+
+
+@app.route('/mise', methods=['GET'])
+def post_mise():
+    url = "http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99"
+    response = requests.get(url)
+    response_data = response.json()
+    return jsonify(response_data)
+
+
+@app.route('/corona', methods=['GET'])
+def post_corona():
+    service_key = "BAsEIVf9MMHmkMVeeuK%2FqFA6vCoojczmlHEhH7OBVKazC8UYA1euOJbbT6R3xEQWo12Dc2sH3JIvg0Jj75bcQA%3D%3D"
+    today = datetime.datetime.now().date().strftime("%Y%m%d")
+    yesterday = (datetime.datetime.now().date() - datetime.timedelta(1)).strftime("%Y%m%d")
+    pageNo = 1
+    numOfRows = 10
+
+    host = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson"
+    url = f"{host}?serviceKey={service_key}&pageNo={pageNo}&numOfRows={numOfRows}&startCreateDt={yesterday}&endCreateDt={today}"
+
+    response = requests.get(url)
+    xtree = xmltodict.parse(response.text)
+    response_data = json.loads(json.dumps(xtree))
+    return jsonify(response_data)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
